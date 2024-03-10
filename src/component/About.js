3bf63998db8ea52aff97a0b33a86d8ca42cd4643
Neil Nahar <naharneil@.com>
2024-03-10 13:45:21 -0500

    row alignment fix

--- src/component/About.js
+++ src/component/About.js
@@ -2,7 +2,7 @@ import UserClass from "./UserClass"
 
 const About = () => {
   return (
-    <div className="grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-2 p-6 gap-6">
+    <div className="grid lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-2 p-6 gap-6 auto-rows-auto">
       <UserClass name={"Aman Jain"} role={"Software Developer"} location={"Delhi"} />
       <UserClass name={"Aman Jain"} role={"Software Developer"} location={"Delhi"} />
       <UserClass name={"Aman Jain"} role={"Software Developer"} location={"Delhi"} />
--- src/component/Body.js
+++ src/component/Body.js
@@ -6,7 +6,7 @@ import useStatus from "../utils/useStatus";
 
 const Card = (props) => {
   return (
-    <div className="rounded-2xl shadow-lg shadow-slate-500 p-3.5 flex flex-col gap-3.5 hover:shadow-slate-700 hover:scale-105">
+    <div className="rounded-2xl shadow-lg shadow-slate-500 h-full p-3.5 flex flex-col gap-3.5 hover:shadow-slate-700 hover:scale-105">
       <img className="h-40 w-full rounded-2xl" src={CDN + props.data.info.cloudinaryImageId} alt="Food Image" />
       <div className="flex justify-between text-sm font-semibold">
         <p>{props.data.info.name}</p>
@@ -84,7 +84,7 @@ const Cardcontainer = () => {
           Sort by Rating
         </button>
       </div>
-      <div className="grid gap-6 px-6 lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-2 auto-rows-max">
+      <div className="grid gap-6 px-6 lg:grid-cols-6 md:grid-cols-4 sm:grid-cols-2 auto-rows-auto">
         {resDataFiltered.map((item) => {
           return (
             <Link to={"/restaurant/" + item.info.id} key={item.info.id}>
